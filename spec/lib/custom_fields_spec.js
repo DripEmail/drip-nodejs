da6163a0f44d843f12c90f2abb4bc7df7166bc18
Robyn-Dale Samuda <robyn@yuraki.com>
2017-12-27 11:16:22 -0600

    Custom fields

--- spec/lib/custom_fields_spec.js
+++ spec/lib/custom_fields_spec.js
@@ -1,35 +1,59 @@
-'use strict';
+const sinon = require('sinon');
+const client = require('../../lib/index')({ token: 'abc123', accountId: 9999999 });
 
-var sinon = require('sinon')
-  , client = require('../../lib/index')({ token: 'abc123' })
-  , request = require('request')
-  , helper = require('../../lib/helpers');
-
-describe('Custom fields', function () {
-  var accountId = 123;
-
-  beforeEach(function () {
-    sinon.stub(request, 'get')
-      .yields(null, { statusCode: 200 }, { accounts : {} }
-    );
+describe('Custom fields with callback', () => {
+  beforeEach(() => {
+    sinon.stub(client, 'request')
+      .yields(null, { statusCode: 200 }, { custom_field_identifiers: {} });
   });
 
-  afterEach(function () {
-    request.get.restore();
+  afterEach(() => {
+    client.request.restore();
   });
 
-  it('should provide the correct base URL', function () {
-    expect(helper.customFieldsUrl(accountId))
-      .toBe('https://api.getdrip.com/v2/123/custom_field_identifiers/')
-  })
-
-  it('should list all account-wide custom fields and call request with get', function (done) {
+  it('should list all account-wide custom fields and call request with get', (done) => {
     expect(typeof client.listAllCustomFields).toEqual('function');
 
-    client.listAllCustomFields(accountId, function(error, response, body) {
+    client.listAllCustomFields((error, response) => {
       expect(response.statusCode).toBe(200);
-      expect(request.get.callCount).toBe(1);
+      expect(client.request.callCount).toBe(1);
     });
     done();
+  });
+});
+
+describe('Custom fields with Promise', () => {
+  const expectedResponse = {
+    statusCode: 200,
+    body: {
+      custom_field_identifiers: [{}]
+    }
+  };
+
+  const failTest = (error) => {
+    expect(error).toBeUndefined();
+  };
+
+  beforeEach(() => {
+    sinon.stub(client, 'request').resolves(expectedResponse);
+    spyOn(client, 'get').and.callThrough();
+  });
+
+  afterEach(() => {
+    client.request.restore();
+  });
+
+  it('should list campaigns', (done) => {
+    expect(typeof client.listAllCustomFields).toEqual('function');
+
+    client.listAllCustomFields()
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(client.request.callCount).toBe(1);
+      })
+      .catch(failTest);
+    done();
+
+    expect(client.get).toHaveBeenCalledWith('9999999/custom_field_identifiers/', {}, undefined);
   });
 });
