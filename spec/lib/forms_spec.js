f775a6eb91efcf9a7aa58b23101e79d6a9ba9a01
Robyn-Dale Samuda <robyn@yuraki.com>
2017-12-27 11:55:30 -0600

    Forms

--- spec/lib/forms_spec.js
+++ spec/lib/forms_spec.js
@@ -1,45 +1,85 @@
-'use strict';
+const sinon = require('sinon');
+const client = require('../../lib/index')({ token: 'abc123', accountId: 9999999 });
 
-var sinon = require('sinon')
-  , client = require('../../lib/index')({ token: 'abc123' })
-  , request = require('request')
-  , helper = require('../../lib/helpers');
+const formId = 5555555;
 
-describe('Forms', function (){
-  var accountId = 123;
-  var formId = 555;
-
-  beforeEach(function (){
-    sinon.stub(request, 'get')
-      .yields(null, { statusCode: 200 }, { accounts : {} }
-    );
+describe('Forms with callback', () => {
+  beforeEach(() => {
+    sinon.stub(client, 'request')
+      .yields(null, { statusCode: 200 }, { forms: {} });
   });
 
-  afterEach(function () {
-    request.get.restore();
+  afterEach(() => {
+    client.request.restore();
   });
 
-  it('should provide the correct base URL', function () {
-    expect(helper.formsUrl(123)).toBe('https://api.getdrip.com/v2/123/forms/')
-  });
-
-  it('should list all forms and call request with get', function (done) {
+  it('should list all forms and call request with get', (done) => {
     expect(typeof client.listForms).toEqual('function');
 
-    client.listForms(accountId, function (error, response, body) {
+    client.listForms((error, response) => {
       expect(response.statusCode).toBe(200);
-      expect(request.get.callCount).toBe(1);
-    });
-    done()
-  });
-
-  it('should fetch a form and call request with get', function (done) {
-    expect(typeof client.fetchForm).toEqual('function');
-
-    client.fetchForm(accountId, formId, function (error, response, body) {
-      expect(response.statusCode).toBe(200);
-      expect(request.get.callCount).toBe(1);
+      expect(client.request.callCount).toBe(1);
     });
     done();
+  });
+
+  it('should fetch a form and call request with get', (done) => {
+    expect(typeof client.fetchForm).toEqual('function');
+
+    client.fetchForm(formId, (error, response) => {
+      expect(response.statusCode).toBe(200);
+      expect(client.request.callCount).toBe(1);
+    });
+    done();
+  });
+});
+
+describe('Forms with promise', () => {
+  const expectedResponse = {
+    statusCode: 200,
+    body: {
+      forms: [{}]
+    }
+  };
+
+  const failTest = (error) => {
+    expect(error).toBeUndefined();
+  };
+
+  beforeEach(() => {
+    sinon.stub(client, 'request').resolves(expectedResponse);
+    spyOn(client, 'get').and.callThrough();
+  });
+
+  afterEach(() => {
+    client.request.restore();
+  });
+
+  it('should list forms', (done) => {
+    expect(typeof client.listForms).toEqual('function');
+
+    client.listForms()
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(client.request.callCount).toBe(1);
+      })
+      .catch(failTest);
+    done();
+
+    expect(client.get).toHaveBeenCalledWith('9999999/forms', {}, undefined);
+  });
+
+  it('should fetch a form', (done) => {
+    expect(typeof client.fetchForm).toEqual('function');
+
+    client.fetchForm(formId)
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(client.request.callCount).toBe(1);
+      })
+      .catch(failTest);
+    done();
+
+    expect(client.get).toHaveBeenCalledWith('9999999/forms/5555555', {}, undefined);
   });
 });
