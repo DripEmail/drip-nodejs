52dadfe347689b675b0052c0f9cb97466d3dd1bd
Robyn-Dale Samuda <robyn@yuraki.com>
2017-12-27 20:47:58 -0600

    Triggers

--- spec/lib/workflow_triggers_spec.js
+++ spec/lib/workflow_triggers_spec.js
@@ -1,60 +1,112 @@
-'use strict';
+const sinon = require('sinon');
+const client = require('../../lib/index')({ token: 'abc123', accountId: 9999999 });
 
-var sinon = require('sinon')
-  , client = require('../../lib/index')({ token: 'abc123' })
-  , request = require('request')
-  , helper = require('../../lib/helpers');
+const workflowId = 444555;
+const workflowTriggerId = 'df1234';
 
-describe('Workflow triggers', function () {
-  var accountId = 123;
-  var workflowId = 456;
-  var triggerId = 999;
-
-  beforeEach(function () {
-    sinon.stub(request, 'get')
-      .yields(null, { statusCode: 200 }, { accounts : {} }
-    );
-    sinon.stub(request, 'post')
-      .yields(null, { statusCode: 204 }, {}
-    );
-    sinon.stub(request, 'put')
-      .yields(null, { statusCode: 200 }, {}
-    );
+describe('Workflow triggers with callback', () => {
+  beforeEach(() => {
+    sinon.stub(client, 'request')
+      .yields(null, { statusCode: 200 }, { accounts: {} });
   });
 
-  afterEach(function () {
-    request.get.restore();
-    request.post.restore();
-    request.put.restore();
+  afterEach(() => {
+    client.request.restore();
   });
 
-  it('should list all workflow triggers and call request with get', function (done) {
+  it('should list all workflow triggers and call request with get', (done) => {
     expect(typeof client.listTriggers).toEqual('function');
 
-    client.listTriggers(accountId, workflowId, function(error, response, body) {
+    client.listTriggers(workflowId, (error, response) => {
       expect(response.statusCode).toBe(200);
-      expect(request.get.callCount).toBe(1);
+      expect(client.request.callCount).toBe(1);
     });
     done();
   });
 
-  it('should create a workflow trigger and call request with post', function (done) {
+  it('should create a workflow trigger and call request with post', (done) => {
     expect(typeof client.createTrigger).toEqual('function');
 
-    client.createTrigger(accountId, workflowId, { "provider": "some_app" }, function (error, response, body) {
-      expect(response.statusCode).toBe(204);
-      expect(request.post.callCount).toBe(1);
+    client.createTrigger(workflowId, { provider: 'some_app' }, (error, response) => {
+      expect(response.statusCode).toBe(200);
+      expect(client.request.callCount).toBe(1);
     });
     done();
   });
 
-  it('should update a workflow trigger and call request with put', function (done) {
+  it('should update a workflow trigger and call request with put', (done) => {
     expect(typeof client.updateTrigger).toEqual('function');
 
-    client.updateTrigger(accountId, workflowId, triggerId, { "provider": "some_other_app" }, function (error, response, body) {
+    client.updateTrigger(workflowId, workflowTriggerId, { provider: 'some_other_app' }, (error, response) => {
       expect(response.statusCode).toBe(200);
-      expect(request.put.callCount).toBe(1);
+      expect(client.request.callCount).toBe(1);
     });
     done();
+  });
+});
+
+describe('Workflows with promise', () => {
+  const expectedResponse = {
+    statusCode: 200,
+    body: {
+      triggers: [{}]
+    }
+  };
+
+  const failTest = (error) => {
+    expect(error).toBeUndefined();
+  };
+
+  beforeEach(() => {
+    sinon.stub(client, 'request').resolves(expectedResponse);
+    spyOn(client, 'get').and.callThrough();
+    spyOn(client, 'post').and.callThrough();
+    spyOn(client, 'put').and.callThrough();
+  });
+
+  afterEach(() => {
+    client.request.restore();
+  });
+
+  it('should list all workflow triggers', (done) => {
+    expect(typeof client.listTriggers).toEqual('function');
+
+    client.listTriggers(workflowId)
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(client.request.callCount).toBe(1);
+      })
+      .catch(failTest);
+    done();
+
+    expect(client.get).toHaveBeenCalledWith('9999999/workflows/444555/triggers', {}, undefined);
+  });
+
+  it('should create a workflow trigger', (done) => {
+    expect(typeof client.createTrigger).toEqual('function');
+
+    client.createTrigger(workflowId, { provider: 'some_app' })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(client.request.callCount).toBe(1);
+      })
+      .catch(failTest);
+    done();
+
+    expect(client.post).toHaveBeenCalledWith('9999999/workflows/444555/triggers', { payload: { provider: 'some_app' } }, undefined);
+  });
+
+  it('should update a workflow trigger', (done) => {
+    expect(typeof client.updateTrigger).toEqual('function');
+
+    client.updateTrigger(workflowId, 'df1234', { provider: 'some_app' })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(client.request.callCount).toBe(1);
+      })
+      .catch(failTest);
+    done();
+
+    expect(client.put).toHaveBeenCalledWith('9999999/workflows/444555/triggers/df1234', { payload: { provider: 'some_app' } }, undefined);
   });
 });
