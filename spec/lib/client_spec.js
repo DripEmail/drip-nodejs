b9f2aa7f1cb0af4c739989130d89e3a36b53a602
Robyn-Dale Samuda <robyn@yuraki.com>
2017-12-28 09:42:17 -0600

    Convert index to new class

--- spec/lib/client_spec.js
+++ spec/lib/client_spec.js
@@ -1,30 +1,29 @@
-const Client = require('../../lib/index');
+const client = require('../../lib/index')({ token: 'abc123', accountId: 9999999 });
+const bearerClient = require('../../lib/index')({ token: 'abc123', tokenType: 'Bearer', accountId: 9999999 });
 const VERSION = require('../../lib/version');
 
+const headers = client.requestHeaders();
+const bearerHeaders = bearerClient.requestHeaders();
+const token = 'abc123';
+
 describe('Client', () => {
-  const token = 'abc123';
   it('should have token attribute', () => {
-    const client = new Client({ token });
     expect(client.token).toEqual('YWJjMTIz');
   });
 
   it('should add content-type header', () => {
-    const client = new Client({ token });
-    expect(client.headers['content-type']).toEqual('application/vnd.api+json');
+    expect(headers['Content-Type']).toEqual('application/vnd.api+json');
   });
 
   it('should add basic auth authorization header when no tokenType is specified', () => {
-    const client = new Client({ token });
-    expect(client.headers.authorization).toEqual(`Basic ${Buffer.from(token).toString('base64')}`);
+    expect(headers.authorization).toEqual(`Basic ${Buffer.from(token).toString('base64')}`);
   });
 
   it('should add bearer auth authorization header when tokenType is "Bearer"', () => {
-    const client = new Client({ token, tokenType: 'Bearer' });
-    expect(client.headers.authorization).toEqual(`Bearer ${token}`);
+    expect(bearerHeaders.authorization).toEqual(`Bearer ${token}`);
   });
 
   it('should add user-agent header', () => {
-    const client = new Client({ token });
-    expect(client.headers['User-Agent']).toEqual(`Drip NodeJS Wrapper ${VERSION}`);
+    expect(headers['User-Agent']).toEqual(`Drip NodeJS Wrapper ${VERSION}`);
   });
 });
